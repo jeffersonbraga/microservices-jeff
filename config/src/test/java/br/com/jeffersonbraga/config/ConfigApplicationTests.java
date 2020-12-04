6f862a936c56dbea324976edae6eb280a2630c37
jeffersonsoftfocus <jefferson.braga@softfocus.com.br>
2020-12-03 22:43:04 -0300

    Config server

---
+++ config/src/test/java/br/com/jeffersonbraga/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeffersonbraga.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
